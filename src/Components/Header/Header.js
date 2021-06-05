50600a75c451679517a447dabc91b6b2d2c8fdd6
zyrass <zyrass@outlook.fr>
2021-06-05 11:35:34 +0200

    Add Greensock
    Ajout de la librairie greensock avec quelques animations. + Modification du numéro de version du package.json + Ajout d'un détails sur markdown pour afficher comme quoi j'utilise greensock

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,46 @@
+// Librairies / Dépendances
+import React, { useEffect } from 'react'
+import gsap from "gsap"
+
+// Module CSS
 import myStyle from "./Header.module.css"
 
 function Header() {
+
+  // Functions
+  const titleGsap = name => document.getElementById(name)
+
+  // 
+  useEffect(() => {
+    gsap.fromTo(titleGsap('h1Start'), {
+      x: "-550px"
+    }, {
+      x: "0",
+      duration: 3
+    })
+    gsap.fromTo(titleGsap("h1End"), {
+      x: "550px"
+    }, {
+      x: "0",
+      duration: 3
+    })
+  })
+
   return (
     <header className={ myStyle.header }>
-        <h1>Markdown generator</h1>
+        <h1>
+          <span 
+            id="h1Start" 
+            className={ myStyle.h1Start }
+          >Markdown</span> 
+
+          &nbsp;
+
+          <span 
+            id="h1End" 
+            className={ myStyle.h1End }
+          >Generator</span> 
+        </h1>
     </header>
   )
 }
--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,5 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import Marked from 'marked';
-import React, { useState } from 'react';
+import gsap from "gsap";
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
 import myStyle from  './App.module.css';
@@ -9,7 +10,7 @@ function App() {
 
   // State
   const [markdown, setMarkdown] = useState(`# Projet Markdown Generator V2\n\n
-  Je suis un super fière de vous **présenter** ce projet.\n\nCe projet est disponible sur mon github à cette [adresse](https://github.com/Zyrass/React-Markdown-V2).\n\n1. Vous pouvez tout effacer, c'est fait pour.\n2. J'espère que ça vous plaira.\n\n\n- Sur Believemy il te donne les directive et c'est à toi de faire tout le code de A à Z.\n- La **V1** est disponible à cette [adresse](https://react-markdown-generator.herokuapp.com/).\n\n\n**Cordialement** **A**lain **G**uillon alias (**Z**yrass)\n\n\n\`PS : Merci à Believemy pour cette idée de projet.\``)
+  Je suis un super fière de vous **présenter** ce projet.\n\nCe projet est disponible sur mon github à cette [adresse](https://github.com/Zyrass/React-Markdown-V2).\n\n1. Vous pouvez tout effacer, c'est fait pour.\n2. J'ai ajouté quelques animations avec la librairie **Greensock** ( **_gsap_** )\n3. J'espère que ça vous plaira.\n\n\n- Sur Believemy il nous donne les directives à suivre et c'est donc à nous de faire le job.\n- La **V1** est disponible à cette [adresse](https://react-markdown-generator.herokuapp.com/).\n\n\n**Cordialement** **A**lain **G**uillon alias (**Z**yrass)\n\n\n\`PS : Merci à Believemy pour cette idée de projet.\``)
 
   // Functions
   const changeMarkdownHandler = event => {
@@ -21,8 +22,22 @@ function App() {
     const data = ( Marked( markdown, {
       breaks: true
     }))
-    return { __html: data }
+
+    return { __html: data}
   }
+
+  useEffect(() => {
+    return (
+      gsap.fromTo(document.getElementById("htmlGenerate"), {
+        scale: 0,
+        opacity: 0
+      }, {
+        scale: 1,
+        opacity: 1,
+        duration: 2
+      })
+    )
+  })
 
   return (
     <div className={myStyle.App}>
@@ -44,6 +59,7 @@ function App() {
 
         <div className={myStyle.right}>
           <div 
+            id="htmlGenerate"
             className={myStyle.showHTML}
             dangerouslySetInnerHTML={ loadHTML() }
           />
